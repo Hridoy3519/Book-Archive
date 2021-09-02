ea6eb09c3c71de38927db734683c47fe404e6350
Hridoy Chowdhury <hridoy3519@gmail.com>
2021-09-02 14:46:53 +0600

    Third commit

--- script/app.js
+++ script/app.js
@@ -1,4 +1,8 @@
 const searchBook = () => {
+
+    //Hide Result Area, until data loaded
+    toggleResultArea('none');
+
     //Getting searched text and erasing the input field.
     const inputField = document.getElementById('input-field');
     searchText = inputField.value;
@@ -16,6 +20,7 @@ const showSearchResult = bookList => {
     resultCount.innerText = `Total Results Found: ${bookList.numFound}`;
 
     //Displaying search Result
+    toggleResultArea('block');
     const resultContainer = document.getElementById('result-container');
     resultContainer.textContent = "";
 
@@ -24,21 +29,35 @@ const showSearchResult = bookList => {
         div.classList.add('col');
 
         //Getting Book cover img url
-        let imgURL = "../images/not-available.png";
+        let imgURL = "../images/not-available.jpg";
         if (book.cover_i) {
-            imgURL = `http://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`;
+            imgURL = `http://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         }
 
+
         div.innerHTML = `
-        <div class="card h-100">
-            <img src=${imgURL} class="card-img-top" onerror="imgError(this);" alt="...">         
-            <div class="card-body">
-                <h5 class="card-title">Title: ${book.title}</h5>
-                <h5 class="card-title">Author: ${book.author_name}</h5>
-                <p class="card-text">Publisher: ${book.publisher?.[0]}</p>
-                <p class="card-text">First Published on: ${book.first_publish_year}</p>
+        <div class="card mb-3 h-100" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src=${imgURL} class="card-img-top" onerror="imgError(this);" alt="..."> 
             </div>
-        </div>`;
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <p class="card-text">
+                    <span class="text-secondary">Author:</span> <span class="text-primary fw-bold">${book.author_name ? book.author_name : 'No Info'}</span > 
+                </p >
+                <p class="card-text">
+                    <span class="text-secondary">Publisher:</span> <span class="text-primary fw-bold">${book.publisher?.[0] ? book.publisher?.[0] : 'No Info'}</span > 
+                </p >
+                <p class="card-text">
+                    <span class="text-secondary">First Published:</span> ${book.first_publish_year ? book.first_publish_year : 'Unknown'}
+                </p >
+            </div >
+            </div >
+        </div >
+    </div >
+    `;
         resultContainer.appendChild(div);
     });
 }
@@ -46,6 +65,10 @@ const showSearchResult = bookList => {
 const imgError = (image) => {
     console.log(image)
     image.onerror = "";
-    image.src = "../images/not-available.png";
+    image.src = "../images/not-available.jpg";
     return true;
+}
+
+const toggleResultArea = (displayStyle) => {
+    document.getElementById('result-area').style.display = displayStyle;
 }
